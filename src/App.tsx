065887503a33b9d0b91ea7e4f2da0eed55a4095c
hisku <hiskias.melke1@gmail.com>
2025-04-14 16:43:45 +0900

    Code cleanup, replace default button from lovable by a custom one

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Simulator from "./pages/Simulator";
